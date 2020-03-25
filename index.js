3cf840be13193b1a37086aaeab31027d7184f69f
Catherine Jimenez <xcatherine.jimenez@gmail.com>
2020-03-25 21:30:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@
 function theBeatlesPlay(musicians, instruments){
   var arr= [];
   for(let i = 0; i < musicians.length; i++){
-    arr.push(musicians[0] + " plays " + instruments[0])
+    arr.push(musicians[i] + " plays " + instruments[i])
   }
 }
