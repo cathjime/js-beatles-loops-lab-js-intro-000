c37977f244fe31c4a0e032f4063c47c71f0a2418
Catherine Jimenez <xcatherine.jimenez@gmail.com>
2020-03-25 21:31:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 // add solution here
 function theBeatlesPlay(musicians, instruments){
-  var arr= [];
+  var arr = [];
   for(let i = 0; i < musicians.length; i++){
     arr.push(musicians[i] + " plays " + instruments[i])
   }
+  return arr;
 }
